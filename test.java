4bb0ba86090d7ec11fedc3145b288c336355403a
ElroyHimself <1102492250@qq.com>
2021-12-08 15:39:30 +0800

    2021/12/08

---
+++ test.java
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2021-12-08 11:08:59
+ * @LastEditors: ELROY
+ * @LastEditTime: 2021-12-08 15:30:53
+ * @FilePath: \Java\test.java
+ */
+// Hello.java
+// Hello.java
+class E {
+    public static void main(String[] args) {
+        System.out.print("I am E Class, which is a main class");
+    }
+}
